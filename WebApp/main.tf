26e6256f331a23e2aff4f8d6bd3af435858b8a6e
Jaron <jdbedwards5@gmail.com>
2026-02-06 09:09:54 -0600

    Update main.tf

--- WebApp/main.tf
+++ WebApp/main.tf
@@ -2,11 +2,11 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   
   name = "WebApp-VPC"
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr_block
   
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = var.availability_zones
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
   
   enable_nat_gateway = true
   enable_vpn_gateway = true
@@ -19,15 +19,15 @@ module "vpc" {
 
 resource "aws_launch_template" "WebApp_LT" {
   name_prefix   = "WebApp"
-  image_id      = "ami-0ed26f8cab1bc3bdb"
-  instance_type = "t2.micro"
+  image_id      = var.image_id
+  instance_type = var.instance_type
   vpc_security_group_ids = [resource.aws_security_group.ec2.id]
 
 }
 
 resource "aws_autoscaling_group" "WebApp_ASG" {
   name                = "WebApp-ASG"
-  vpc_zone_identifier = module.vpc.private_subnets  # FIXED: use subnets not AZs
+  vpc_zone_identifier = module.vpc.private_subnets
   desired_capacity    = 1
   max_size            = 2
   min_size            = 0
@@ -40,16 +40,13 @@ resource "aws_autoscaling_group" "WebApp_ASG" {
 
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"  # CHANGED: Use version 8 instead of 9
-  
+  version = "~> 8.0"
   name               = "WebApp-ALB"
   load_balancer_type = "application"
   vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
-  
-  security_groups = [aws_security_group.alb.id]  # CHANGED: Use separate SG
-  
-  target_groups = [  # CHANGED: List syntax for v8
+  security_groups = [aws_security_group.alb.id]
+  target_groups = [
     {
       name_prefix      = "webapp"
       backend_protocol = "HTTP"
@@ -57,8 +54,7 @@ module "alb" {
       target_type      = "instance"
     }
   ]
-  
-  http_tcp_listeners = [  # CHANGED: v8 listener syntax
+  http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
@@ -105,7 +101,8 @@ resource "aws_security_group" "ec2" {
   }
 }
 
+#Attach autoscaling group to load balancer target group
 resource "aws_autoscaling_attachment" "WebApp_attachment" {
   autoscaling_group_name = aws_autoscaling_group.WebApp_ASG.name
-  lb_target_group_arn    = module.alb.target_group_arns[0]  # CHANGED: v8 output
+  lb_target_group_arn    = module.alb.target_group_arns[0]
 }
